7eecb5fbd5b6b82ea224ed5d6c4c9e6543d3483d
MrBrom <t.abram94@gmail.com>
2022-02-08 02:06:25 +0100

    Changes to the status class

--- src/main/java/com/tabram/coffemaker/Main.java
+++ src/main/java/com/tabram/coffemaker/Main.java
@@ -5,15 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         MachineAction.turnOnCaffeeMaker();
-
-
         MachineAction.turnOffCaffeeMaker();
-
-
-
-
-//      Map<String, String> checkStatus = new Map<String, String>();
-//        System.out.println(checkStatus <StatuEnums.WATER, Status_2.waterStatus>);
 
 
     }
